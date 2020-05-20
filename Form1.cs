1f92432c6630dcb2f6769f82b58c13538043b4c4
Datorium <hello@datorium.lv>
2020-05-20 17:30:40 +0300

    version 13:30

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,13 @@ namespace TicTacToe
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightCoral;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
         }
     }
 }
