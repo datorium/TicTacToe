72485d1c22bc3112405730fa04a631a927bacb89
Datorium <hello@datorium.lv>
2020-05-23 18:00:23 +0300

    version 18:00

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,7 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
-
         bool xPlayerTurn = true;
-
         public Form1()
         {
             InitializeComponent();
@@ -41,6 +40,12 @@ namespace TicTacToe
         private void Player_Click(object sender, EventArgs e)
         {
             Label label = (Label)sender;
+            
+            if(label.Text != string.Empty)
+            {
+                return;
+            }
+            
             if (xPlayerTurn)
             {
                 label.Text = "X";
@@ -49,7 +54,39 @@ namespace TicTacToe
             {
                 label.Text = "O";
             }
+            CheckForWin();
             xPlayerTurn = !xPlayerTurn;         
+        }
+
+        private void CheckForWin()
+        {
+            if (
+                    (label1.Text == label2.Text && label2.Text == label3.Text && label1.Text != string.Empty) ||
+                    (label4.Text == label5.Text && label5.Text == label6.Text && label4.Text != string.Empty) ||
+                    (label7.Text == label8.Text && label8.Text == label9.Text && label7.Text != string.Empty) ||
+                    (label1.Text == label4.Text && label4.Text == label7.Text && label1.Text != string.Empty) ||
+                    (label2.Text == label5.Text && label5.Text == label8.Text && label2.Text != string.Empty) ||
+                    (label3.Text == label6.Text && label6.Text == label9.Text && label3.Text != string.Empty) ||
+                    (label1.Text == label5.Text && label5.Text == label9.Text && label1.Text != string.Empty) ||
+                    (label3.Text == label5.Text && label5.Text == label7.Text && label3.Text != string.Empty)
+                )
+            {
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            string winner;
+            if (xPlayerTurn)
+            {
+                winner = "X";
+            }
+            else
+            {
+                winner = "O";
+            }
+            MessageBox.Show(winner  + " wins!");
         }
     }
 }
